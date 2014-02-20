cba341aab90821f856f4fb613f8627e7d7c2f412
Michael Barrett <loki77@gmail.com>
2014-02-19 17:44:42 -0800

    Update version, stop importing
    This sucks, I need to find a way to track the version in only one place,
    but I had to stop importing __init__.py or else setup.py had a
    requirement on boto, which is lame.
    
    I'll figure this out later.

--- setup.py
+++ setup.py
@@ -5,19 +5,13 @@ from setuptools import setup, find_packages
 src_dir = os.path.dirname(__file__)
 
 
-def get_version(src_dir):
-    sys.path.insert(0, src_dir)
-    import aws_helper
-    return aws_helper.__version__
-
-
 def read(filename):
     full_path = os.path.join(src_dir, filename)
     with open(full_path) as fd:
         return fd.read()
 
 setup(name='aws_helper',
-      version=get_version(src_dir),
+      version='0.2.1',
       author='Michael Barrett',
       author_email='loki77@gmail.com',
       description='Helpers for dealing with AWS services.',
